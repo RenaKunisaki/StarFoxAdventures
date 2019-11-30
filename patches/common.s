ee2965e84c7ffce0fa6d68ac1df7cef3bb9cc35a
Rena Kunisaki <hyperhacker@gmail.com>
2019-11-29 21:36:17 -0500

    add common.s, oops

--- patches/common.s
+++ patches/common.s
@@ -6,6 +6,13 @@
 .set r20,20; .set r21,21; .set r22,22; .set r23,23; .set r24,24
 .set r25,25; .set r26,26; .set r27,27; .set r28,28; .set r29,29
 .set r30,30; .set r31,31; .set f0,0; .set f2,2; .set f3,3
+.set f0,0;   .set f1,1;   .set f2,2; .set f3,3;   .set f4,4
+.set f5,5;   .set f6,6;   .set f7,7;   .set f8,8;   .set f9,9
+.set f10,10; .set f11,11; .set f12,12; .set f13,13; .set f14,14
+.set f15,15; .set f16,16; .set f17,17; .set f18,18; .set f19,19
+.set f20,20; .set f21,21; .set f22,22; .set f23,23; .set f24,24
+.set f25,25; .set f26,26; .set f27,27; .set f28,28; .set f29,29
+.set f30,30; .set f31,31; .set f0,0; .set f2,2; .set f3,3
 
 .set PAD_BUTTON_LEFT, 0x0001
 .set PAD_BUTTON_RIGHT,0x0002
