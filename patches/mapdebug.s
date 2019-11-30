abee8dd5716f9afcb755113747bfd5fb8f2e3b66
Rena Kunisaki <hyperhacker@gmail.com>
2019-11-29 21:45:29 -0500

    fix mapdebug

--- patches/mapdebug.s
+++ patches/mapdebug.s
@@ -28,8 +28,8 @@ beq     end
 
 LOAD    r4, 0x8032CC05 # "%04x%04x"
 lis     r5, 0x803e
-lhz     r6, (0x10000 - 0xcdcc)(r5) # cell Z
-lhz     r5, (0x10000 - 0xcdc8)(r5) # cell X
+lhz     r6, -(0x10000 - 0xcdce)(r5) # cell Z
+lhz     r5, -(0x10000 - 0xcdca)(r5) # cell X
 CALL    sprintf
 
 end:
