6259c39facdbbcc6440ee11c71e9e81cf890a1ff
Rena Kunisaki <hyperhacker@gmail.com>
2019-12-04 13:24:17 -0500

    alloc debug: trigger crash screen

--- patches/allocdebug.s
+++ patches/allocdebug.s
@@ -8,7 +8,12 @@ GECKO_BEGIN_PATCH 0x80023864 # or r23, r3, r3
 srwi    r23, r4, 16
 cmpwi   r23, 0x0180
 blt     end
-.byte   0, 0, 0, 1 # an invalid opcode
+
+# deliberately crash
+lis     r23, 0xD06F
+ori     r23, r23, 0xECE5
+lis     r8, 0
+stw     r23, 0(r8)
 
 end:
 or      r23, r3, r3 # replaced
