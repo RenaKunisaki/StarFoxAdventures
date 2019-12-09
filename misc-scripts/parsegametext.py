f6cd59877aa414a86e4d33a341c3974d621fcc4a
Rena Kunisaki <hyperhacker@gmail.com>
2019-12-09 13:38:11 -0500

    add script to parse gametext files

---
+++ misc-scripts/parsegametext.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+import sys
+import json
+import struct
+
+with open(sys.argv[1], 'rb') as file:
+    numCharStructs = struct.unpack('>I', file.read(4))[0] # grumble
+    charStructs = []
+    for i in range(numCharStructs):
+        fields = struct.unpack('>IHHbbbbBBBB', file.read(16))
+        charStructs.append({
+            'character': fields[ 0],
+            'xpos':      fields[ 1],
+            'ypos':      fields[ 2],
+            'left':      fields[ 3],
+            'right':     fields[ 4],
+            'top':       fields[ 5],
+            'bottom':    fields[ 6],
+            'width':     fields[ 7],
+            'height':    fields[ 8],
+            'font':      fields[ 9],
+            'texture':   fields[10],
+        })
+
+    numTexts, unknown = struct.unpack('>HH', file.read(4))
+    #print("numTexts=", numTexts, "unknown=", unknown)
+
+    texts = {}
+    for i in range(numTexts):
+        id, nPhrases, window, alignH, alignV, lang, offset = struct.unpack(
+            '>HHBBBBI', file.read(12))
+        texts[id] = {
+            'nPhrases': nPhrases,
+            'window':   window,
+            'alignH':   alignH,
+            'alignV':   alignV,
+            'language': lang,
+            'phrases':  offset,
+        }
+
+    numStrs = struct.unpack('>I', file.read(4))[0] # grumble
+    #print("numStrs=", numStrs, "at 0x%X" % (file.tell() - 2))
+    offsets = []
+    for i in range(numStrs):
+        offsets.append(struct.unpack('>I', file.read(4))[0]) # grumble
+
+    strs = []
+    base = file.tell()
+    for i in range(numStrs):
+        file.seek(base + offsets[i])
+        s = []
+        while True:
+            b = file.read(1)[0]
+            if b == 0xEF: # control code
+                c = file.read(1)[0]
+                if c == 0xA3:
+                    c = file.read(1)[0]
+                    if c == 0xB4:
+                        # set styles or something
+                        # used for area names, "no controller" msg
+                        c = file.read(2)
+                        b = "<STYLE %02X%02X>" % (c[0], c[1])
+                    elif c == 0xB7:
+                        # switch font texture
+                        # used to display buttons and faces
+                        # 2: buttons, 4: default, 5: faces
+                        c = file.read(2)
+                        b = "<FONT %02X%02X>" % (c[0], c[1])
+                    else:
+                        c = file.read(2)
+                        b = '<UNK %02X%02X>' % (c[0], c[1])
+                else: b = '\\xEF\\x%02X' % c
+            elif b == 0: break
+
+            if type(b) is not str:
+                if b > 127 or b < 32: b = "\\x%02X" % b
+                else: b = chr(b)
+            s.append(b)
+        strs.append(''.join(s))
+
+    for id, text in texts.items():
+        #print("%04X W=%02X H=%02X V=%02X L=%02X P=%04X" % (
+        #    id, text['window'], text['alignH'], text['alignV'],
+        #    text['language'], text['nPhrases']
+        #))
+        phrases = []
+        for i in range(text['nPhrases']):
+            #print("", '"' + strs[i+text['phrases']] + '"')
+            phrases.append(strs[i+text['phrases']])
+        text['phrases'] = phrases
+
+    print(json.dumps({
+        'unknown': unknown,
+        'charStructs': charStructs,
+        'texts': texts,
+    }, sort_keys=True, indent=4))
---
+++ notes/gametext.md
@@ -0,0 +1,50 @@
+`gametext/<mapname>/<language>.bin` is a different format from root `gametext.bin`
+that one might not even be used
+
+u32 number of entries
+characterStruct[] entries, 16 bytes each up to 0x5C4
+
+after that, looks like 12-byte structs, probably gametextStruct
+
+ID   nPhr Wn aH aV Ln phrases
+0000 0005 2D 00 03 00 00000000  
+004C 0007 7C 03 00 00 00000005  
+004D 0007 7C 03 00 00 0000000C  
+
+0x0	0x2	GameTextId	identifier	which game text is this
+0x2	0x2	ushort	numPhrases	how many phrases this text has
+0x4	0x1	typedef u8 byte	window	preferred window
+0x5	0x1	typedef u8 byte	alignH	horizontal alignment
+0x6	0x1	typedef u8 byte	alignV	vertical alignment
+0x7	0x1	GameLanguageEnum	language	LANGUAGE_xxx from GameTextData.h
+0x8	0x4	char * *	phrases	pointer to <numPhrases> pointers to localised strings
+
+from 0x5C4 to 0xCC0 = 0x6FC bytes = 0x95 (149) entries
+but at 0x93C the ID resets to zero (0x378 bytes, 0x4A (74) entries)
+at 0xCC0 the actual text begins
+
+actually I guess they begin at 0x5C8
+also the offset seems to be an index into a string table
+I think when the ID is 0, that's the beginning of the table
+after the ID you have 00DF which is probably number of strings
+and then offsets to each: 0x00, 0x09, 0x0E, 0x18...
+
+00DF is at 0x942, strings at 0xCC0, so 0x37E bytes = 0xDF (223) strings plus the 00DF itself
+
+so the entire file is:
+
+u32 num_char_structs;
+characterStruct[num_char_structs];
+
+u16 numTexts;
+u16 unknown;
+gametextStruct[numTexts] texts;
+
+u32 numStrings;
+u32 strOffset[numStrings];
+char[] strings[numStrings];
+
+FrontEnd contains the credits, some unused cheats (Xmas Mode), "Play full game", "GAME SELECT", "PREVIOUSLY ON", Slippy's hints
+AnimTest contains only the generic menu texts from every map
+Boot contains only disc read error messages and "Loading..."
+Sequences contains a ton of files... maybe only one text each
