f17e6b532adae5eb07bf96817b9a15f9c55136af
Rena Kunisaki <hyperhacker@gmail.com>
2020-06-04 20:24:42 -0400

    add patch for better pause menu

--- patches/makeall.sh
+++ patches/makeall.sh
@@ -8,8 +8,9 @@ TARGET=~/projects/sfa/files
 ./makedolpatch.sh debugprint && mv -f debugprint.bin $TARGET/patches/0000
 ./makedolpatch.sh krystal && mv -f krystal.bin $TARGET/patches/0001
 ./makedolpatch.sh climb && mv -f climb.bin $TARGET/patches/0002
-./makedolpatch.sh alloc && mv -f alloc.bin $TARGET/patches/0003
+./makedolpatch.sh pausemenu && mv -f pausemenu.bin $TARGET/patches/0003
+./makedolpatch.sh alloc && mv -f alloc.bin $TARGET/patches/0004
 
 # extract Krystal model and texture to own files
-dd if=$TARGET/animtest/MODELS.bin of=$TARGET/patches/km bs=1 skip=210720 count=76608
-dd if=$TARGET/animtest/TEX1.bin of=$TARGET/patches/kt bs=1 skip=288864 count=104960
+#dd if=$TARGET/animtest/MODELS.bin of=$TARGET/patches/km bs=1 skip=210720 count=76608
+#dd if=$TARGET/animtest/TEX1.bin of=$TARGET/patches/kt bs=1 skip=288864 count=104960
