9f000b6fcb682caeca8f66c69814e639e98f83d1
Rena Kunisaki <hyperhacker@gmail.com>
2020-06-06 10:49:19 -0400

    GameBit patch: only log if changed

--- patches/gamebit.s
+++ patches/gamebit.s
@@ -12,8 +12,13 @@ patchList:
     PATCH_END PATCH_KEEP_AFTER_RUN
 
 constants:
-    .set STACK_SIZE,0x20 # how much to reserve
-    .set SP_LR_SAVE,0x10
+    .set STACK_SIZE, 0x30 # how much to reserve
+    .set SP_LR_SAVE, 0x34
+    .set SP_R3_SAVE, 0x10
+    .set SP_R4_SAVE, 0x14
+    .set SP_R14_SAVE,0x18
+    .set SP_R15_SAVE,0x1C
+    .set SP_R16_SAVE,0x20
 
 entry: # called as soon as our patch is loaded.
     blr # nothing to do here
@@ -23,6 +28,11 @@ main:
     stwu r1, -STACK_SIZE(r1) # get some stack space
     mflr r9
     stw  r9,  SP_LR_SAVE(r1)
+    stw  r3,  SP_R3_SAVE(r1)
+    stw  r4,  SP_R4_SAVE(r1)
+    stw  r14, SP_R14_SAVE(r1)
+    stw  r15, SP_R15_SAVE(r1)
+    stw  r16, SP_R16_SAVE(r1)
 
     bl .getpc
     .getpc:
@@ -38,24 +48,37 @@ main:
     bne   .nextIgnore
 
 .noIgnore:
-    mr   r5, r4
-    mr   r4, r3
+    # is value being changed?
+    CALL mainGetBit
+    mr   r5, r3  # r5 = current val
+    lwz  r4,  SP_R3_SAVE(r1) # r4 = bit
+    lwz  r6,  SP_R4_SAVE(r1) # r6 = new val
+
+    cmpw r5, r6
+    beq  .end # no change
+
+    # changed, so print msg
     addi r3, r14, (.msg - .getpc)@l
     CALL OSReport
 
 .end:
     lwz  r4, SP_LR_SAVE(r1)
     mtlr r4 # restore LR
+    lwz  r3,  SP_R3_SAVE(r1)
+    lwz  r4,  SP_R4_SAVE(r1)
+    lwz  r14, SP_R14_SAVE(r1)
+    lwz  r15, SP_R15_SAVE(r1)
+    lwz  r16, SP_R16_SAVE(r1)
     addi r1, r1, STACK_SIZE # restore stack ptr
     JUMP 0x800e87b0, r4 # isSaveGameLoading
 
 .ignoreList: # these change pretty much every frame
     .short 0x7511 # dummy entry
-    .short 0x0960, 0x0961, 0x0964, 0x0965, 0x0969, 0x096B, 0x0986
-    .short 0x0A7F, 0x0A8B
-    .short 0x0EB5
-    .short 0x0F10, 0x0F46
+    #.short 0x0960, 0x0961, 0x0964, 0x0965, 0x0969, 0x096B, 0x0986
+    #.short 0x0A7F, 0x0A8B
+    #.short 0x0EB5
+    #.short 0x0F10, 0x0F46
     .short 0
 
 .msg:
-    .string "Bit %04X=%X"
+    .string "Bit %04X %X -> %X"
