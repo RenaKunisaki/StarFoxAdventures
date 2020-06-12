1ec89a8d71d6c97ca3fad331de259ce503bfbce4
Rena Kunisaki <hyperhacker@gmail.com>
2020-06-12 19:28:39 -0400

    tidying

--- patches/pausemenu.s
+++ patches/pausemenu.s
@@ -21,8 +21,3 @@ patchList:
 
 entry: # called as soon as our patch is loaded.
     blr # nothing to do here
-
-# eventually we should put a menu in that pops up with some key combo,
-# or replaces the pause menu or even has its own icon (could use Krystal talking head model)
-# and has options like toggle debug print, set FOV, game speed,
-# cheats, etc.
