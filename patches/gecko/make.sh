15969dbeda2fd9eeb058464d26629ebe7a6987ed
Rena Kunisaki <hyperhacker@gmail.com>
2020-06-12 19:03:30 -0400

    fix gecko make script

--- patches/gecko/make.sh
+++ patches/gecko/make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 TOOL=/opt/devkitpro/devkitPPC/bin/powerpc-eabi
 
-$TOOL-as -mbroadway -o $1.elf $1.s
+$TOOL-as -I ../include -mbroadway -mregnames --fatal-warnings -o $1.elf $1.s
 $TOOL-objcopy -O binary $1.elf $1.bin
 ./bin2gecko.py $1.bin $1.gecko
 rm $1.elf $1.bin
