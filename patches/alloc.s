5a2e21a18ab9dd48ea8787cdbeeac2a7fd4efdfb
Rena Kunisaki <hyperhacker@gmail.com>
2020-06-23 15:20:41 -0400

    allocdebug: fixes, improvements

--- patches/alloc.s
+++ patches/alloc.s
@@ -33,7 +33,7 @@ main:
         mflr r31
 
     addi r3, r31, (.msg - .getpc)@l
-    mr   r4, r0  # lr
+    mr   r4, r9  # lr
     mr   r5, r28 # size
     mr   r6, r29 # tag
     mr   r7, r30 # name
@@ -52,8 +52,10 @@ main:
     lwz  r4, SP_LR_SAVE(r1)
     mtlr r4 # restore LR
     lwz  r28, SP_R28_SAVE(r1)
-    lwz  r29, SP_R29_SAVE(r1)
+    #lwz  r29, SP_R29_SAVE(r1)
     lwz  r30, SP_R30_SAVE(r1)
+    mr   r29, r4 # replace tag with lr
+    # not name, because it isn't stored
     addi r1, r1, STACK_SIZE # restore stack ptr
     li   r0, 1 # replaced
     JUMP 0x80023d04, r31 # return to original code
