27c4b13c4010b78f791ac80ab05d84b8c4d999d6
Rena Kunisaki <hyperhacker@gmail.com>
2020-07-03 21:32:07 -0400

    add autosave

---
+++ patches/autosave.s
@@ -0,0 +1,45 @@
+# autosave patch:
+# 1) saves the game when moving between maps
+# 2) removes "saving" message so that isn't so bothersome
+# NOTE: if you swap memory cards (or load a save state) this patch
+# will not show the "not same card" message but also will not save
+# and the game will remove the save option from the pause menu.
+.text
+.include "common.s"
+
+# define patches
+patchList:
+    PATCH_ID "AutoSav" # must be 7 chars
+    PATCH_BL 0x80042ec4, main
+    # disable message
+    PATCH_WORD 0x8007db50, 0x60000000
+    PATCH_WORD 0x8007db94, 0x60000000
+    PATCH_END PATCH_KEEP_AFTER_RUN
+
+constants:
+    .set STACK_SIZE,0x100 # how much to reserve
+    .set SP_LR_SAVE,0x104 # this is what the game does
+    .set SP_GPR_SAVE,0x20
+
+entry: # called as soon as our patch is loaded.
+    # nothing to do
+    blr
+
+main:
+    # called when about to load another map.
+    stwu    r1, -STACK_SIZE(r1) # get some stack space
+    mflr    r5
+    stw     r5, SP_LR_SAVE(r1)
+    stmw    r3, SP_GPR_SAVE(r1)
+
+    LOADWH  r3, curSaveSlot
+    LOADBL2 r4, curSaveSlot, r3
+    cmpwi   r4, 0xFF
+    beq     .end # no save slot
+    CALL    0x800e86d0 # save
+.end:
+    lwz     r5, SP_LR_SAVE(r1)
+    mtlr    r5 # restore LR
+    lmw     r3, SP_GPR_SAVE(r1)
+    addi    r1, r1, STACK_SIZE # restore stack ptr
+    JUMP    0x800443cc, r0 # mapLoadDataFile
--- patches/makeall.sh
+++ patches/makeall.sh
@@ -3,19 +3,24 @@
 # you will probably want to change the destination
 # and maybe the order
 TARGET=~/projects/sfa/files
+#TARGET=~/projects/games/hax/sfa/root/files
 
 set -e
+mkdir -p $TARGET/patches
 ./makedolpatch.sh debugbin && mv -f debugbin.bin $TARGET/debug.bin
 ./makedolpatch.sh debugprint && mv -f debugprint.bin $TARGET/patches/0000
 ./makedolpatch.sh krystal && mv -f krystal.bin $TARGET/patches/0001
-./makedolpatch.sh climb && mv -f climb.bin $TARGET/patches/0002
-./makedolpatch.sh pausemenu && mv -f pausemenu.bin $TARGET/patches/0003
-./makedolpatch.sh pda && mv -f pda.bin $TARGET/patches/0004
-./makedolpatch.sh freemove && mv -f freemove.bin $TARGET/patches/0005
-#./makedolpatch.sh alloc && mv -f alloc.bin $TARGET/patches/0006
-#./makedolpatch.sh gamebit && mv -f gamebit.bin $TARGET/patches/0006
-./makedolpatch.sh hitboxdebug && mv -f hitboxdebug.bin $TARGET/patches/0006
-./makedolpatch.sh dll && mv -f dll.bin $TARGET/patches/0007
+./makedolpatch.sh cmenu && mv -f cmenu.bin $TARGET/patches/0002
+./makedolpatch.sh climb && mv -f climb.bin $TARGET/patches/0003
+./makedolpatch.sh pausemenu && mv -f pausemenu.bin $TARGET/patches/0004
+./makedolpatch.sh pda && mv -f pda.bin $TARGET/patches/0005
+./makedolpatch.sh freemove && mv -f freemove.bin $TARGET/patches/0006
+./makedolpatch.sh alloc && mv -f alloc.bin $TARGET/patches/0007
+#./makedolpatch.sh gamebit && mv -f gamebit.bin $TARGET/patches/0008
+./makedolpatch.sh hitboxdebug && mv -f hitboxdebug.bin $TARGET/patches/0008
+#./makedolpatch.sh dll && mv -f dll.bin $TARGET/patches/0009
+./makedolpatch.sh staff_fx && mv -f staff_fx.bin $TARGET/patches/0009
+./makedolpatch.sh autosave && mv -f autosave.bin $TARGET/patches/0010
 
 # extract Krystal model and texture to own files
 dd if=$TARGET/animtest/MODELS.bin of=$TARGET/patches/km1 bs=1 skip=210720 count=76608
