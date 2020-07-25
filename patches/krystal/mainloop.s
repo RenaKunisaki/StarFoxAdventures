87a844dee49735f43409514cebd9e72cbc5c3dba
Rena Kunisaki <hyperhacker@gmail.com>
2020-07-25 16:27:24 -0400

    krystal: patch AnimFox (fix Drakor scene)

--- patches/krystal/mainloop.s
+++ patches/krystal/mainloop.s
@@ -36,34 +36,45 @@ mainLoopPatch:
 
     # set correct model for AnimFoxLink object, which is the "afterimage"
     # during the WarpStone sequence.
-    LOADWH  r16, dataFileBuffers
-    LOADWL2 r4,  dataFileBuffers+(OBJECTS_TAB*4), r16 # get OBJECTS.tab address
-    LOADWL2 r5,  dataFileBuffers+(OBJECTS_BIN*4), r16 # get OBJECTS.bin address
-    cmpwi   r4,  0
-    beq     .mainLoopPatch_end
-    cmpwi   r5,  0
-    beq     .mainLoopPatch_end
-
-    lwz     r6,  0x1220(r4) # get AnimFoxLink offset
-    add     r6,  r6,  r5    # get address
-    lwz     r5,  0x08(r6)   # get model list
-    cmpwi   r5,  0
-    blt     .mainLoopPatch_noModelOffsTrans
-    # r5 is an offset, not an address
-    add     r5,  r5,  r6
-.mainLoopPatch_noModelOffsTrans:
-
-    slwi    r7,  r15, 1
-    addi    r8,  r20, modelIds - mainLoopPatch
-    lhzx    r8,  r7,  r8 # get model ID
-    stw     r8,  0(r5) # set object's model ID
-
+    li      r3,  0x1220
+    bl      mainLoopPatchObjModel
+    li      r3,  0x121C # AnimFox, for Drakor scene
+    bl      mainLoopPatchObjModel
 
 .mainLoopPatch_end:
     lwz     r3, SP_LR_SAVE(r1)
     mtlr    r3
     lmw     r3, SP_GPR_SAVE(r1)
     addi    r1, r1, STACK_SIZE # restore stack ptr
+    blr
+
+
+mainLoopPatchObjModel:
+    # patch model ID of given object
+    # r3 = offset (not index) of object's pointer in OBJECTS.TAB
+    LOADWH  r16, dataFileBuffers
+    LOADWL2 r4,  dataFileBuffers+(OBJECTS_TAB*4), r16 # get OBJECTS.tab address
+    LOADWL2 r5,  dataFileBuffers+(OBJECTS_BIN*4), r16 # get OBJECTS.bin address
+    cmpwi   r4,  0
+    beqlr
+    cmpwi   r5,  0
+    beqlr
+
+    #lwz     r6,  0x1220(r4) # get AnimFoxLink offset
+    lwzx    r6,  r4,  r3
+    add     r6,  r6,  r5    # get address
+    lwz     r5,  0x08(r6)   # get model list
+    cmpwi   r5,  0
+    blt     .mainLoopPatchObjModel_noModelOffsTrans
+    # r5 is an offset, not an address
+    add     r5,  r5,  r6
+    .mainLoopPatchObjModel_noModelOffsTrans:
+
+    slwi    r7,  r15, 1
+    addi    r8,  r20, modelIds - mainLoopPatch
+    lhzx    r8,  r7,  r8 # get model ID
+    stw     r8,  0(r5) # set object's model ID
+
     blr
 
 
