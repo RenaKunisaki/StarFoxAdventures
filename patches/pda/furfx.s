57b9722cdaffd69efc81181e34ea19934f9b098e
Rena Kunisaki <hyperhacker@gmail.com>
2020-08-08 14:29:55 -0400

    furfx: more optimization

--- patches/pda/furfx.s
+++ patches/pda/furfx.s
@@ -10,32 +10,36 @@ drawItem_furEffect:
     blr
 
 adjItem_furEffect: # r3 = amount to adjust by (0=A button)
-    addi    r3,  r14,  (.furFx_addrs  - mainLoop) - 4
-    addi    r4,  r14,  (.furFx_values - mainLoop) - 4
-    li      r5,  1     # number of values
-.adjItem_furEffect_next:
-    lwzu    r6,  4(r3) # get addr
-    lwzu    r7,  4(r4) # get XOR
-    lwz     r8,  0(r6) # get current value
-    xor     r8,  r8,  r7
-    stw     r8,  0(r6)
+#    addi    r3,  r14,  (.furFx_addrs  - mainLoop) - 4
+#    addi    r4,  r14,  (.furFx_values - mainLoop) - 4
+#    li      r5,  1     # number of values
+#.adjItem_furEffect_next:
+#    lwzu    r6,  4(r3) # get addr
+#    lwzu    r7,  4(r4) # get XOR
+#    lwz     r8,  0(r6) # get current value
+#    xor     r8,  r8,  r7
+#    stw     r8,  0(r6)
+    LOAD    r6,  0x800414E0
+    lwz     r7,  0(r6)
+    xori    r7,  r7,  0x1312
+    stw     r7,  0(r6)
     li      r8,  0
     icbi    r8,  r6 # flush instruction cache
-    subi    r5,  r5,  1
-    cmpwi   r5,  0
-    bne     .adjItem_furEffect_next
+#    subi    r5,  r5,  1
+#    cmpwi   r5,  0
+#    bne     .adjItem_furEffect_next
     blr
 
-.furFx_addrs:
+#.furFx_addrs:
     #.int 0x800414E4
-    .int 0x800414E0
+#    .int 0x800414E0
     #.int 0x80041518
     #.int 0x800415D0
     #.int 0x80041640
     #.int 0x8004164C
-.furFx_values: # original XOR replacement
+#.furFx_values: # original XOR replacement
     #.int 0x40820028 ^ 0x48000028
-    .int 0x2C000000 ^ 0x2C001312
+#    .int 0x2C000000 ^ 0x2C001312
     # are any of these necessary?
     #.int 0x41820020 ^ 0x48000020
     #.int 0x4082003C ^ 0x4800003C
