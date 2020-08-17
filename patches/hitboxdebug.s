58d2320dfdd510a4952c6f675d161aed3b80f80a
Rena Kunisaki <hyperhacker@gmail.com>
2020-08-17 13:42:11 -0400

    hitboxdebug: seems to draw OK

--- patches/hitboxdebug.s
+++ patches/hitboxdebug.s
@@ -185,7 +185,7 @@ drawHitbox:
     blr
 
 f_0:  .float 0.0
-f_1:  .float 1.0
+f_1:  .float 10.0
 
 drawSphere:
     # f1, f2, f3 = X, Y, Z
@@ -194,7 +194,8 @@ drawSphere:
     # expects r14 = .drawHitbox_getpc, r29 = 0xCC010000
     .set NUM_SPHERE_STACKS, 4
     .set NUM_SPHERE_SLICES, 4
-    .set NUM_SPHERE_VTXS, NUM_SPHERE_STACKS * NUM_SPHERE_SLICES
+    .set NUM_SPHERE_FACES, NUM_SPHERE_STACKS * NUM_SPHERE_SLICES
+    .set NUM_SPHERE_VTXS,  NUM_SPHERE_FACES  * 4
     .set SIZEOF_FLOAT, 4
     .set SIZEOF_VEC3F, SIZEOF_FLOAT * 3
 
@@ -210,100 +211,29 @@ drawSphere:
     #addi    r7,  r14, sphereVtxs - .drawHitbox_getpc
     #li      r8,  NUM_SPHERE_VTXS
 
-    li      r15, 1 # stack
-.drawSphere_nextStack:
-    li      r17, 0
+    lis     r29, 0xCC01
+    li      r15, 0    # vtx idx
+    addi    r17, r14, sphereVtxs    - .drawHitbox_getpc
+    addi    r18, r14, sphereVtxIdxs - .drawHitbox_getpc
+.drawSphere_nextSlice:
+    li      r16, 0    # stack idx
     li      r3,  0x98 # DRAW_TRIANGLE_STRIP
     li      r4,  2  # vat
-    li      r5,  NUM_SPHERE_SLICES * 4 # nvtxs
+    li      r5,  NUM_SPHERE_STACKS * NUM_SPHERE_SLICES # nvtxs
     CALL    gxBeginVtxs
-
-    li      r16, 1 # slice
-    lis     r29, 0xCC01
-.drawSphere_nextSlice:
-
-    # note we draw vtxs out of order: 1, 3, 2, 4
-
-    # int v1 = (stack * nSlices) + slice;
-    addi    r7,  r14, sphereVtxs - .drawHitbox_getpc
-    mulli   r3,  r15, NUM_SPHERE_SLICES # v1 = stack * nSlices
-    add     r3,  r3,  r16 # v1 += slice
+.drawSphere_nextVtx:
+    lbzx    r3,  r18, r15 # get vtx idx
     mulli   r3,  r3,  SIZEOF_VEC3F
-    add     r3,  r3,  r7  # v1 = &vtxs[v1]
-    mr      r4,  r3       # save for later
-    bl      .drawSphere_drawVtx
-    addi    r17, r17, 1
-
-    # int v3 = v1 - nSlices;
-    subi    r3,  r4,  NUM_SPHERE_SLICES * SIZEOF_VEC3F
-    bl      .drawSphere_drawVtx
-    addi    r17, r17, 1
-
-    # int v2 = v1 - 1;
-    subi    r3,  r4,  SIZEOF_VEC3F
-    bl      .drawSphere_drawVtx
-    addi    r17, r17, 1
-
-    # int v4 = v3 - 1;
-    # v3 is v1 - nSlices, so v4 = v1 - (nSlices + 1)
-    subi    r3,  r4,  (NUM_SPHERE_SLICES + 1) * SIZEOF_VEC3F
-    bl      .drawSphere_drawVtx
-    addi    r17, r17, 1
-
-    addi    r16, r16, 1 # slice++
-    cmpwi   r16, NUM_SPHERE_SLICES+1
-    blt     .drawSphere_nextSlice
-
-    addi    r15, r15, 1 # stack++
-    cmpwi   r15, NUM_SPHERE_STACKS+1
-    blt     .drawSphere_nextStack
-
-    # reuse function epilogue to save a few bytes
-    b       .drawHitbox_end
-
-.drawSphere_drawVtx:
-    # params: r3=&vtx, f1=X, f2=Y, f3=Z, f4=radius
-    # clobbers: r3, f5, f6, f7
-    #li      r3,  0
-    #stw     r3,  -0x8000(r29)
-    #stw     r3,  -0x8000(r29)
-    #stw     r3,  -0x8000(r29)
-    #stb     r3,  -0x8000(r29)
-    #stb     r3,  -0x8000(r29)
-    #stb     r3,  -0x8000(r29)
-    #stb     r3,  -0x8000(r29)
-    #blr
-
-    stwu    r1,  -STACK_SIZE(r1) # get some stack space
-    stmw    r3,  SP_GPR_SAVE(r1)
-    mflr    r7
-    stw     r7,  SP_LR_SAVE(r1)
-
+    add     r3,  r3,  r17 # v1 = &vtxs[v1]
     lfs     f5,  0(r3)
     lfs     f6,  4(r3)
     lfs     f7,  8(r3)
-    #fmuls   f5,  f5,  f4
-    #fmuls   f6,  f6,  f4
-    #fmuls   f7,  f7,  f4
-    #fadds   f5,  f5,  f1
-    #fadds   f6,  f6,  f2
-    #fadds   f7,  f7,  f3
-
-    fmr     f8,  f1
-    fmr     f9,  f2
-    fmr     f10, f3
-    fmr     f1,  f5
-    fmr     f2,  f6
-    fmr     f3,  f7
-    mr      r4,  r3
-    addi    r3,  r14, s_vtx - .drawHitbox_getpc
-    mr      r5,  r17
-    mr      r6,  r15
-    mr      r7,  r16
-    CALL    OSReport
-    fmr     f1,  f8
-    fmr     f2,  f9
-    fmr     f3,  f10
+    fmuls   f5,  f5,  f4
+    fmuls   f6,  f6,  f4
+    fmuls   f7,  f7,  f4
+    fadds   f5,  f5,  f1
+    fadds   f6,  f6,  f2
+    fadds   f7,  f7,  f3
 
     stfs    f5,  -0x8000(r29) # X
     stfs    f6,  -0x8000(r29) # Y
@@ -316,18 +246,17 @@ drawSphere:
     stb     r3,  -0x8000(r29) # B
     lwz     r3,  SP_SAVE_A(r1)
     stb     r3,  -0x8000(r29) # A
-    #blr
-    b       .drawHitbox_end
+
+    addi    r15, r15, 1
+    cmpwi   r15, NUM_SPHERE_VTXS
+    beq     .drawHitbox_end # reuse function epilogue to save a few bytes
+
+    addi    r16, r16, 1
+    cmpwi   r16, NUM_SPHERE_STACKS * NUM_SPHERE_SLICES
+    beq     .drawSphere_nextSlice
+    b       .drawSphere_nextVtx
 
 sphereVtxs: # precomputed by sphere.py
-    .float  0,  1,  2,  3,  4,  5,  6,  7,  8,  9
-    .float 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
-    .float 20, 21, 22, 23, 24, 25, 26, 27, 28, 29
-    .float 30, 31, 32, 33, 34, 35, 36, 37, 38, 39
-    .float 40, 41, 42, 43, 44, 45, 46, 47, -48, -49
-    .float -50, -51, -52, -53, -54, -55, -56, -57, -58, -59
-    .float -60, -61, -62, -63, -64, -65, -66, -67, -68, -69
-
     .float  0.00000, -1.00000,  0.00000
     .float  0.00000, -1.00000,  0.00000
     .float  0.00000, -1.00000,  0.00000
@@ -344,6 +273,27 @@ sphereVtxs: # precomputed by sphere.py
     .float  0.86603,  0.50000,  0.00000
     .float  0.00000,  0.50000, -0.86603
     .float -0.86603,  0.50000,  0.00000
+
+sphereVtxIdxs: # unwrapped vtx idxs
+    .byte  0,  1,  4,  5
+    .byte  1,  2,  5,  6
+    .byte  2,  3,  6,  7
+    .byte  3,  0,  7,  4
+
+    .byte  4,  5,  8,  9
+    .byte  5,  6,  9, 10
+    .byte  6,  7, 10, 11
+    .byte  7,  4, 11,  8
+
+    .byte  8,  9, 12, 13
+    .byte  9, 10, 13, 14
+    .byte 10, 11, 14, 15
+    .byte 11,  8, 15, 12
+
+    .byte 12, 13,  0,  1
+    .byte 13, 14,  1,  2
+    .byte 14, 15,  2,  3
+    .byte 15, 12,  3,  0
 
 s_vtx: .string "V %08X %d %d,%d %f %f %f sc %f"
 .endif # ENABLE_DRAW_HITBOX
