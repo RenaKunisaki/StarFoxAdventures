018cf4dc682eccda398b3b0d629c2627239ab69a
Rena Kunisaki <hyperhacker@gmail.com>
2021-02-17 20:57:51 -0500

    formatting

--- patches/debugbin.s
+++ patches/debugbin.s
@@ -56,7 +56,16 @@
 # 816820e8
 # 80003ebc
 
-.set DEBUG,0
+# patchinfo.bin format:
+# u32 magic;     //0x52656E61
+# u32 numFiles;  //number of patch files to load
+# u32 totalSize; //total size to allocate for patch files
+# u8  verMajor;  //major version number
+# u8  verMinor;  //minor version number
+# u16 verRev;    //revision number
+# u32 buildNo;   //build number
+
+.set DEBUG,1
 .text
 .include "common.s"
 .include "globals.s"
@@ -69,19 +78,19 @@ constants:
     .set STARTUP_HOOK_ADDR,0x80021038 # bl installBsodHandlers
     .set PAD_HOOK_ADDR,0x80014f90 # 482398D5 bl padReadControllers
 
-    .set OP_WRITE8,0x00
-    .set OP_WRITE16,0x02
-    .set OP_WRITE32,0x04
+    .set OP_WRITE8,  0x00
+    .set OP_WRITE16, 0x02
+    .set OP_WRITE32, 0x04
     .set OP_ADD_HOOK,0xC0
-    .set OP_BRANCH,0xC6
-    .set OP_NOP,0xFE
+    .set OP_BRANCH,  0xC6
+    .set OP_NOP,     0xFE
 
-    .set STACK_SIZE,0xC0 # how much to reserve
-    .set SP_LR_SAVE,0xC4
-    .set SP_TMP1,0x10
+    .set STACK_SIZE,   0xC0 # how much to reserve
+    .set SP_LR_SAVE,   0xC4
+    .set SP_TMP1,      0x10
     .set SP_PATCH_ADDR,0x14
     .set SP_FLAGS_ADDR,0x18
-    .set SP_GPR_SAVE,0x1C
+    .set SP_GPR_SAVE,  0x1C
 
 start:
     # entry point of this file.
@@ -89,11 +98,10 @@ start:
     # this is called very early in init, so we may not want to run
     # most patches yet.
 
-    mr   r14, r3 # save address of this code
-    stwu r1, -STACK_SIZE(r1) # get some stack space
-    mflr r3
-    stw  r3,  SP_LR_SAVE(r1)
-    #stw  r0,  SP_R0_SAVE(r1) # XXX needed?
+    mr      r14, r3 # save address of this code
+    stwu    r1,  -STACK_SIZE(r1) # get some stack space
+    mflr    r3
+    stw     r3,  SP_LR_SAVE(r1)
 
     # say something to show we're alive.
     .if DEBUG
@@ -102,155 +110,154 @@ start:
     .endif
 
     # set our state pointer
-    addi   r3, r14, patchStateVars - start
-    LOADWH r4, PATCH_STATE_PTR
-    STOREW r3, PATCH_STATE_PTR, r4
+    addi    r3,  r14, patchStateVars - start
+    LOADWH  r4,  PATCH_STATE_PTR
+    STOREW  r3,  PATCH_STATE_PTR, r4
 
     # install startup hook.
-    addi   r5, r14, (startHook - start)@l # absolute destination address
-    LOAD   r4, STARTUP_HOOK_ADDR
-    sub    r7, r5, r4 # r7 = relative destination addr
-    rlwinm r7, r7, 0, 1, 29 # r7 = r7 & 0x07FFFFFC
-    oris   r7, r7, 0x4800
-    ori    r7, r7, 1 # make it a bl
-    stw    r7, 0(r4)
-    li     r7, 0
-    icbi   r7, r4
+    addi    r5,  r14, (startHook - start)@l # absolute destination address
+    LOAD    r4,  STARTUP_HOOK_ADDR
+    sub     r7,  r5,  r4 # r7 = relative destination addr
+    rlwinm  r7,  r7,  0, 1, 29 # r7 = r7 & 0x07FFFFFC
+    oris    r7,  r7,  0x4800
+    ori     r7,  r7,  1 # make it a bl
+    stw     r7,  0(r4)
+    li      r7,  0
+    icbi    r7,  r4
 
     # install main loop hook.
-    addi   r5, r14, (mainLoop - start)@l # absolute destination address
-    LOAD   r4, MAIN_LOOP_HOOK_ADDR
-    sub    r7, r5, r4 # r7 = relative destination addr
-    rlwinm r7, r7, 0, 1, 29 # r7 = r7 & 0x07FFFFFC
-    oris   r7, r7, 0x4800
-    ori    r7, r7, 1 # make it a bl
-    stw    r7, 0(r4)
-    li     r7, 0
-    icbi   r7, r4
+    addi    r5,  r14, (mainLoop - start)@l # absolute destination address
+    LOAD    r4,  MAIN_LOOP_HOOK_ADDR
+    sub     r7,  r5,  r4 # r7 = relative destination addr
+    rlwinm  r7,  r7,  0, 1, 29 # r7 = r7 & 0x07FFFFFC
+    oris    r7,  r7,  0x4800
+    ori     r7,  r7,  1 # make it a bl
+    stw     r7,  0(r4)
+    li      r7,  0
+    icbi    r7,  r4
 
     # install pad hook.
-    addi   r5, r14, (padHook - start)@l # absolute destination address
-    LOAD   r4, PAD_HOOK_ADDR
-    sub    r7, r5, r4 # r7 = relative destination addr
-    rlwinm r7, r7, 0, 1, 29 # r7 = r7 & 0x07FFFFFC
-    oris   r7, r7, 0x4800
-    ori    r7, r7, 1 # make it a bl
-    stw    r7, 0(r4)
-    li     r7, 0
-    icbi   r7, r4
+    addi    r5,  r14, (padHook - start)@l # absolute destination address
+    LOAD    r4,  PAD_HOOK_ADDR
+    sub     r7,  r5,  r4 # r7 = relative destination addr
+    rlwinm  r7,  r7,  0, 1, 29 # r7 = r7 & 0x07FFFFFC
+    oris    r7,  r7,  0x4800
+    ori     r7,  r7,  1 # make it a bl
+    stw     r7,  0(r4)
+    li      r7,  0
+    icbi    r7,  r4
 
 
     # load patches
     # since we don't have DVDOpenDir or other dir functions,
     # we'll number them.
-    li    r15, 0 # patch idx
+    li      r15, 0 # patch idx
 .nextPatch:
-    addi    r3, r14, (.patchPath - start)@l + 8 # offset of "000000"
-    addi    r4, r14, (.patchFmt - start)@l # format
-    mr      r5, r15 # idx
+    addi    r3,  r14, (.patchPath - start)@l + 8 # offset of "000000"
+    addi    r4,  r14, (.patchFmt - start)@l # format
+    mr      r5,  r15 # idx
     addi    r15, r15, 1
     CALL    sprintf # now patchPath = path to load
-    addi    r3, r14, (.patchPath - start)@l
-    li      r4, 0 # we don't need size
+    addi    r3,  r14, (.patchPath - start)@l
+    li      r4,  0 # we don't need size
     CALL    loadFileByPath
-    cmpwi   r3, 0
+    cmpwi   r3,  0
     beq     .done # file doesn't exist
 
     # r3 = patch file
     # interpret the patch list
-    stw     r3, SP_PATCH_ADDR(r1) # save to free later
-    subi    r3, r3, 8
+    stw     r3,  SP_PATCH_ADDR(r1) # save to free later
+    subi    r3,  r3,  8
 .nextListItem:
-    lwzu    r4, 8(r3) # get first word and advance to next item
-    cmpwi   r4, 0
+    lwzu    r4,  8(r3) # get first word and advance to next item
+    cmpwi   r4,  0
     beq     .endList
     # get command in r5
     # rotate left 8 to move high byte to low byte
     # then mask off low bit and high bytes
     # remember PPC has bit 0 = MSB
-    rlwinm  r5, r4, 8, 24, 30 # shift, mask start, mask end
+    rlwinm  r5,  r4,  8, 24, 30 # shift, mask start, mask end
 
     # convert r4 to address
-    rlwinm  r4, r4, 0, 7, 31 # mask out command
-    oris    r4, r4, 0x8000 # r4 = addr (set high bit)
+    rlwinm  r4,  r4,  0, 7, 31 # mask out command
+    oris    r4,  r4,  0x8000 # r4 = addr (set high bit)
 
     # check command
     # XXX make a table
-    cmpwi   r5, OP_WRITE8
+    cmpwi   r5,  OP_WRITE8
     beq     .write8
-    cmpwi   r5, OP_WRITE16
+    cmpwi   r5,  OP_WRITE16
     beq     .write16
-    cmpwi   r5, OP_WRITE32
+    cmpwi   r5,  OP_WRITE32
     beq     .write32
-    cmpwi   r5, OP_BRANCH
+    cmpwi   r5,  OP_BRANCH
     beq     .makeBl
-    cmpwi   r5, OP_ADD_HOOK
+    cmpwi   r5,  OP_ADD_HOOK
     beq     .addHook
-    cmpwi   r5, OP_NOP
+    cmpwi   r5,  OP_NOP
     beq     .nextListItem
 
     # unsupported command...
     #b       .nextListItem
-    tw       2, r0, r0 # trap if r0 == r0 (ie always)
-    # XXX that doesn't actually do anything...
+    ba       0xDEAD0
 
 .write8: # r4 = address; r3 -> patch list entry
-    lhz     r9, 4(r3) # get count
-    lbz     r6, 7(r3) # get value
-    addi    r9, r9, 1
-    mr      r5, r9 # copy count
+    lhz     r9,  4(r3) # get count
+    lbz     r6,  7(r3) # get value
+    addi    r9,  r9,  1
+    mr      r5,  r9 # copy count
 
 .write8_next: # write bytes in loop
-    subi    r5, r5, 1
-    stbx    r6, r4, r5
-    cmpwi   r5, 0
+    subi    r5,  r5,  1
+    stbx    r6,  r4,  r5
+    cmpwi   r5,  0
     bne     .write8_next
     b       .write_flush
 
 .write16: # r4 = address; r3 -> patch list entry
-    lhz     r9, 4(r3) # get count
-    lhz     r6, 6(r3) # get value
-    addi    r9, r9, 1
-    slwi    r9, r9, 1 # double count
-    mr      r5, r9 # copy count
+    lhz     r9,  4(r3) # get count
+    lhz     r6,  6(r3) # get value
+    addi    r9,  r9,  1
+    slwi    r9,  r9,  1 # double count
+    mr      r5,  r9 # copy count
 
 .write16_next: # write halfwords in loop
-    subi    r5, r5, 2
-    sthx    r6, r4, r5
-    cmpwi   r5, 0
+    subi    r5,  r5,  2
+    sthx    r6,  r4,  r5
+    cmpwi   r5,  0
     bne     .write16_next
     b       .write_flush
 
 .write32: # r4 = address; r3 -> patch list entry
-    lwz     r6, 4(r3) # get value
-    stw     r6, 0(r4) # store value
-    li      r9, 4 # number of bytes to flush
+    lwz     r6,  4(r3) # get value
+    stw     r6,  0(r4) # store value
+    li      r9,  4 # number of bytes to flush
 
 .write_flush:
     # flush cache
     # this is also necessary to tell Dolphin to recompile changed code
-    mr     r28, r3
-    mr     r3,  r4 # addr
-    mr     r4,  r9 # count
-    CALL   iCacheFlush
-    mr     r3,  r28
-    b      .nextListItem
+    mr      r28, r3
+    mr      r3,  r4 # addr
+    mr      r4,  r9 # count
+    CALL    iCacheFlush
+    mr      r3,  r28
+    b       .nextListItem
 
 
 .makeBl: # r4 = address; r3 -> patch list entry
     # low bit of address = whether to make a branch (0) or bl (1)
-    lwz    r5, 4(r3) # get value
-    lwz    r6, SP_PATCH_ADDR(r1) # get patch base address
-    add    r5, r5, r6 # convert patch offset to absolute address
-    mr     r26, r5 # for debug log
+    lwz     r5,  4(r3) # get value
+    lwz     r6,  SP_PATCH_ADDR(r1) # get patch base address
+    add     r5,  r5,  r6 # convert patch offset to absolute address
+    mr      r26, r5 # for debug log
 
-    sub    r7, r5, r4 # r7 = relative destination addr
-    rlwinm r7, r7, 0, 1, 29 # r7 = r7 & 0x07FFFFFC
-    oris   r7, r7, 0x4800 # make it a branch
-    andi.  r5, r5, 1
-    or     r7, r7, r5 # optionally make it a bl
-    oris   r4, r4, 0x8000 # map patched address to 8xxxxxxx
-    stw    r7, 0(r4)
+    sub     r7,  r5,  r4 # r7 = relative destination addr
+    rlwinm  r7,  r7,  0, 1, 29 # r7 = r7 & 0x07FFFFFC
+    oris    r7,  r7,  0x4800 # make it a branch
+    andi.   r5,  r5,  1
+    or      r7,  r7,  r5 # optionally make it a bl
+    oris    r4,  r4,  0x8000 # map patched address to 8xxxxxxx
+    stw     r7,  0(r4)
 
     # debug log
     .if DEBUG
@@ -266,40 +273,40 @@ start:
 
 
 .addHook: # r4 = address; r3 -> patch list entry
-    addi   r7, r14, ((mainLoopHooks - start) - 4)@l
-    lwz    r5,  0(r3) # get opcode
-    andi.  r6,  r5, 3
-    cmpwi  r6,  0
-    beq    .addMainLoopHook
-    cmpwi  r6,  2
-    beq    .addPadHook
+    addi    r7,  r14, ((mainLoopHooks - start) - 4)@l
+    lwz     r5,  0(r3) # get opcode
+    andi.   r6,  r5, 3
+    cmpwi   r6,  0
+    beq     .addMainLoopHook
+    cmpwi   r6,  2
+    beq     .addPadHook
 
     # add startup hook
-    addi   r7, r14, ((startupHooks - start) - 4)@l
+    addi    r7,  r14, ((startupHooks - start) - 4)@l
 
 .addMainLoopHook: # r4 = address; r3 -> patch list entry
-    lwz    r5, 4(r3) # get value
-    lwz    r6, SP_PATCH_ADDR(r1) # get patch base address
-    add    r5, r5, r6 # convert patch offset to absolute address
+    lwz     r5,  4(r3) # get value
+    lwz     r6,  SP_PATCH_ADDR(r1) # get patch base address
+    add     r5,  r5,  r6 # convert patch offset to absolute address
 
 .tryNextHook:
-    lwzu   r6, 4(r7)
-    cmpwi  r6, 0
-    bne    .tryNextHook
-    stw    r5, 0(r7)
-    b      .nextListItem
+    lwzu    r6,  4(r7)
+    cmpwi   r6,  0
+    bne     .tryNextHook
+    stw     r5,  0(r7)
+    b       .nextListItem
     # XXX ensure we don't run out of hooks
 
 .addPadHook:
-    addi   r7, r14, ((padHooks - start) - 4)@l
-    b      .addMainLoopHook
+    addi    r7,  r14, ((padHooks - start) - 4)@l
+    b       .addMainLoopHook
 
 
 .endList:
     # r3 now points to the terminating entry of the patch list
     # which is int 0, int flags. grab the flags
-    lwz    r4, 4(r3)
-    stw    r4, SP_FLAGS_ADDR(r1)
+    lwz     r4,  4(r3)
+    stw     r4,  SP_FLAGS_ADDR(r1)
 
     # now execute the patch
     addi   r17, r3, 8
@@ -310,89 +317,88 @@ start:
         CALL   OSReport
     .endif
 
-    lwz    r3, SP_PATCH_ADDR(r1) # pass base address to patch code.
-    mtspr  CTR, r17
+    lwz     r3,  SP_PATCH_ADDR(r1) # pass base address to patch code.
+    mtspr   CTR, r17
     bctrl
 
     # now handle the flags, of which there's only one.
-    lwz    r4, SP_FLAGS_ADDR(r1)
-    andi.  r4, r4, 1
-    beq    .noFree
+    lwz     r4,  SP_FLAGS_ADDR(r1)
+    andi.   r4,  r4,  1
+    beq     .noFree
 
     # flag 0x01: free this patch after executing it.
     .if DEBUG
-        addi   r3, r14, (.s_free - start)@l
-        addi   r4, r15, -1
+        addi   r3,  r14, (.s_free - start)@l
+        addi   r4,  r15, -1
         CALL   OSReport
     .endif
-    lwz    r3, SP_PATCH_ADDR(r1)
-    CALL   mm_free
+    lwz     r3,  SP_PATCH_ADDR(r1)
+    CALL    mm_free
 
 
 .noFree:
-    b .nextPatch
+    b       .nextPatch
 
 .done:
-    #lwz  r0, SP_R0_SAVE(r1)
-    lwz  r3, SP_LR_SAVE(r1)
-    mtlr r3 # restore LR
-    addi r1, r1, STACK_SIZE # restore stack ptr
+    lwz     r3,  SP_LR_SAVE(r1)
+    mtlr    r3  # restore LR
+    addi    r1,  r1,  STACK_SIZE # restore stack ptr
     blr
 
 
 startHook: # called from startup
-    stwu r1, -STACK_SIZE(r1) # get some stack space
-    stmw r3,  SP_GPR_SAVE(r1)
-    mflr r3
-    stw  r3,  SP_LR_SAVE(r1)
-    CALL 0x80137d28 # installBsodHandlers() (replaced instr)
-    li   r15, (startupHooks - .mainLoop_getpc) - 4
-    b    .mainLoopDoHooks
+    stwu    r1,  -STACK_SIZE(r1) # get some stack space
+    stmw    r3,  SP_GPR_SAVE(r1)
+    mflr    r3
+    stw     r3,  SP_LR_SAVE(r1)
+    CALL    0x80137d28 # installBsodHandlers() (replaced instr)
+    li      r15, (startupHooks - .mainLoop_getpc) - 4
+    b       .mainLoopDoHooks
 
 padHook: # called from pad update
-    stwu r1, -STACK_SIZE(r1) # get some stack space
-    stmw r3,  SP_GPR_SAVE(r1)
-    mflr r4
-    stw  r4,  SP_LR_SAVE(r1)
-    CALL 0x8024e864 # padReadControllers() (replaced instr)
-    lwz  r4,  SP_GPR_SAVE(r1) # pass output pointer as param
-    stw  r3,  SP_TMP1(r1) # save result
-    li   r15, (padHooks - .mainLoop_getpc) - 4
-    bl   .mainLoopDoHooks
+    stwu    r1,  -STACK_SIZE(r1) # get some stack space
+    stmw    r3,  SP_GPR_SAVE(r1)
+    mflr    r4
+    stw     r4,  SP_LR_SAVE(r1)
+    CALL    0x8024e864 # padReadControllers() (replaced instr)
+    lwz     r4,  SP_GPR_SAVE(r1) # pass output pointer as param
+    stw     r3,  SP_TMP1(r1) # save result
+    li      r15, (padHooks - .mainLoop_getpc) - 4
+    bl      .mainLoopDoHooks
 
-    lwz  r4, SP_LR_SAVE(r1)
-    mtlr r4 # restore LR
-    lwz  r3, SP_TMP1(r1) # restore result
-    addi r1, r1, STACK_SIZE # restore stack ptr
+    lwz     r4,  SP_LR_SAVE(r1)
+    mtlr    r4   # restore LR
+    lwz     r3,  SP_TMP1(r1) # restore result
+    addi    r1,  r1,  STACK_SIZE # restore stack ptr
     blr
 
 
 mainLoop: # called from main loop
-    stwu r1, -STACK_SIZE(r1) # get some stack space
-    stmw r3,  SP_GPR_SAVE(r1)
-    mflr r3
-    stw  r3,  SP_LR_SAVE(r1)
-    li   r15, (mainLoopHooks - .mainLoop_getpc) - 4
+    stwu    r1,  -STACK_SIZE(r1) # get some stack space
+    stmw    r3,  SP_GPR_SAVE(r1)
+    mflr    r3
+    stw     r3,  SP_LR_SAVE(r1)
+    li      r15, (mainLoopHooks - .mainLoop_getpc) - 4
 
 .mainLoopDoHooks:
-    bl .mainLoop_getpc
+    bl      .mainLoop_getpc
     .mainLoop_getpc:
         mflr r14
-    add  r15, r15, r14
+    add     r15, r15, r14
 
 .callNextHook:
-    lwzu  r3, 4(r15)
-    cmpwi r3, 0
-    beq   .mainLoopDone
-    mtspr  CTR, r3
+    lwzu    r3,  4(r15)
+    cmpwi   r3,  0
+    beq     .mainLoopDone
+    mtspr   CTR, r3
     bctrl
-    b     .callNextHook
+    b       .callNextHook
 
 .mainLoopDone:
-    lwz  r3, SP_LR_SAVE(r1)
-    mtlr r3 # restore LR
-    lmw    r3,  SP_GPR_SAVE(r1)
-    addi r1, r1, STACK_SIZE # restore stack ptr
+    lwz     r3,  SP_LR_SAVE(r1)
+    mtlr    r3   # restore LR
+    lmw     r3,  SP_GPR_SAVE(r1)
+    addi    r1,  r1,  STACK_SIZE # restore stack ptr
     blr
 
 
@@ -416,6 +422,7 @@ patchStateVars:
     .byte 0
     .endr
 
+.patchInfoPath: .string "patches/patchinfo.bin"
 .patchPath: .string "patches/0000"
 .patchFmt:  .string "%04d"
 .if DEBUG
