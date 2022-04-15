dd97de7809f88f28c18a5a319ece432d6000ea18
Rena Kunisaki <hyperhacker@gmail.com>
2022-04-15 13:00:02 -0400

    move object render logic to new file

--- browser/r/js/app/ui/MapViewer/MapViewer.js
+++ browser/r/js/app/ui/MapViewer/MapViewer.js
@@ -12,6 +12,7 @@ import LayerChooser from "./LayerChooser.js";
 import InfoWidget from "./InfoWidget.js";
 import RenderBatch from "../gl/gx/RenderBatch.js";
 import EventHandler from "./EventHandler.js";
+import ObjectRenderer from "./ObjectRenderer.js";
 
 export default class MapViewer {
     /** Renders map geometry. */
@@ -33,12 +34,12 @@ export default class MapViewer {
             hiddenObjects:      false, //normally hidden objects
         };
 
-        this.grid         = new Grid(this);
-        this.stats        = new Stats(this);
-        this.layerChooser = new LayerChooser(this);
-        this.infoWidget   = new InfoWidget(this);
-        this.eSidebar     = E.div('sidebar');
-        this._eventHandler= new EventHandler(this);
+        this.grid            = new Grid(this);
+        this.stats           = new Stats(this);
+        this.layerChooser    = new LayerChooser(this);
+        this.infoWidget      = new InfoWidget(this);
+        this.eSidebar        = E.div('sidebar');
+        this._eventHandler   = new EventHandler(this);
 
         this._pickerIds     = {};
         this._prevMousePos  = [0, 0];
@@ -78,6 +79,7 @@ export default class MapViewer {
         );
 
         this.blockRenderer = new BlockRenderer(this.gx);
+        this._objectRenderer = new ObjectRenderer(this);
         this._reloadMap();
     }
 
@@ -197,7 +199,7 @@ export default class MapViewer {
         const blockStats = {totals:{}};
         this._beginRender();
         this._drawBlocks(blockStats, blockStreams);
-        this._drawObjects();
+        this._objectRenderer.drawObjects();
         this._finishRender(blockStats, blockStreams);
         //console.log("block render OK", this.gx.context.stats);
         //console.log("GX logs:", this.gx.program.getLogs());
@@ -305,109 +307,6 @@ export default class MapViewer {
             }
         }
         return batch; //for stats
-    }
-
-    _drawObjects() {
-        const gx  = this.gx;
-        const gl  = this.gx.gl;
-        const act = this.layers.actNo;
-        if(act == 0) return;
-        if(!this.map.romList) return;
-
-        const batch = new RenderBatch(this.gx);
-
-        let mv = mat4.clone(gx.context.matModelView);
-        mat4.translate(mv, mv, vec3.fromValues(
-            (this.map.originX * MAP_CELL_SIZE), 0,
-            (this.map.originZ * MAP_CELL_SIZE) ));
-        gx.setModelViewMtx(mv);
-
-        gl.enable(gl.CULL_FACE);
-        gx.setBlendMode(GX.BlendMode.BLEND, GX.BlendFactor.SRCALPHA,
-            GX.BlendFactor.INVSRCALPHA, GX.LogicOp.NOOP);
-        gx.setZMode(true, GX.CompareMode.LEQUAL, true);
-
-        batch.addFunction(() => {
-            for(let i=0; i<this.gx.MAX_TEXTURES; i++) {
-                gl.activeTexture(gl.TEXTURE0 + i);
-                this.gx.whiteTexture.bind();
-                gl.uniform1i(this.gx.programInfo.uniforms.uSampler[i], i);
-            }
-        });
-
-        for(let entry of this.map.romList.entries) {
-            if(act == 'all' || entry.acts[act]) {
-                batch.addFunction(this._drawObject(entry));
-            }
-        }
-        this.gx.executeBatch(batch);
-    }
-
-    _drawObject(entry) {
-        /** Draw an object.
-         *  @param {RomListEntry} entry Object to draw.
-         */
-        const gl = this.gx.gl;
-        const batch = new RenderBatch(this.gx);
-        const x = entry.position.x;
-        const y = entry.position.y;
-        const z = entry.position.z;
-        const s = Math.max(entry.object.scale, 10);
-        //console.log("draw obj", entry, "at", x, y, z, "scale", s)
-
-        let id = 0;
-        if(this._isDrawingForPicker) {
-            id = this._nextPickerId++;
-            this._pickerIds[id] = {
-                type: 'object',
-                obj: entry,
-            };
-        }
-
-        //just draw a cube
-        batch.addVertices(...this._makeCube(x, y, z, s, id));
-        return batch;
-    }
-
-    _makeCube(x, y, z, scale, id) {
-        const vtxPositions = [ //x, y, z
-            [x-scale, y-scale, z-scale], //[0] top left back
-            [x+scale, y-scale, z-scale], //[1] top right back
-            [x+scale, y+scale, z-scale], //[2] top right front
-            [x-scale, y+scale, z-scale], //[3] top left front
-            [x-scale, y-scale, z+scale], //[4] bot left back
-            [x+scale, y-scale, z+scale], //[5] bot right back
-            [x+scale, y+scale, z+scale], //[6] bot right front
-            [x-scale, y+scale, z+scale], //[7] bot left front
-        ];
-        const vtxColors = [ //r, g, b, a
-            [0x00, 0x00, 0x00, 0xCF], //[0] top left back
-            [0xFF, 0x00, 0x00, 0xCF], //[1] top right back
-            [0xFF, 0xFF, 0x00, 0xCF], //[2] top right front
-            [0x00, 0xFF, 0x00, 0xCF], //[3] top left front
-            [0x00, 0x00, 0xFF, 0xCF], //[4] bot left back
-            [0xFF, 0x00, 0xFF, 0xCF], //[5] bot right back
-            [0xFF, 0xFF, 0xFF, 0xCF], //[6] bot right front
-            [0x00, 0xFF, 0xFF, 0xCF], //[7] bot left front
-        ];
-        const vtxIdxs = [
-            0,1,3, 1,2,3, //top
-            3,2,6, 3,6,7, //front
-            0,3,4, 3,7,4, //left
-            2,1,5, 2,5,6, //right
-            7,5,4, 7,6,5, //bottom
-            1,0,5, 0,4,5, //back
-        ];
-        const vtxs = [this.gx.gl.TRIANGLES];
-        for(const idx of vtxIdxs) {
-            vtxs.push({
-                POS:  vtxPositions[idx],
-                COL0: vtxColors[idx],
-                COL1: vtxColors[idx],
-                id:   id,
-            });
-        }
-        return vtxs;
     }
 
     _getObjAt(x, y) {
---
+++ browser/r/js/app/ui/MapViewer/ObjectRenderer.js
@@ -0,0 +1,117 @@
+import { MAP_CELL_SIZE } from "../../../game/Game.js";
+import RenderBatch from "../gl/gx/RenderBatch.js";
+import GX from "../gl/gx/GX.js";
+
+export default class ObjectRenderer {
+    /** Handles object rendering for map viewer. */
+    constructor(mapViewer) {
+        this.mapViewer = mapViewer;
+        this.gx = this.mapViewer.gx;
+    }
+
+    drawObjects() {
+        /** Draw all enabled objects. */
+        const gx  = this.gx;
+        const gl  = this.gx.gl;
+        const act = this.mapViewer.layers.actNo;
+        if(act == 0) return;
+
+        const map = this.mapViewer.map;
+        if(!map.romList) return;
+
+        const batch = new RenderBatch(this.gx);
+
+        let mv = mat4.clone(gx.context.matModelView);
+        mat4.translate(mv, mv, vec3.fromValues(
+            (map.originX * MAP_CELL_SIZE), 0,
+            (map.originZ * MAP_CELL_SIZE) ));
+        gx.setModelViewMtx(mv);
+
+        gl.enable(gl.CULL_FACE);
+        gx.setBlendMode(GX.BlendMode.BLEND, GX.BlendFactor.SRCALPHA,
+            GX.BlendFactor.INVSRCALPHA, GX.LogicOp.NOOP);
+        gx.setZMode(true, GX.CompareMode.LEQUAL, true);
+
+        batch.addFunction(() => {
+            for(let i=0; i<this.gx.MAX_TEXTURES; i++) {
+                gl.activeTexture(gl.TEXTURE0 + i);
+                this.gx.whiteTexture.bind();
+                gl.uniform1i(this.gx.programInfo.uniforms.uSampler[i], i);
+            }
+        });
+
+        for(let entry of map.romList.entries) {
+            if(act == 'all' || entry.acts[act]) {
+                batch.addFunction(this.drawObject(entry));
+            }
+        }
+        this.gx.executeBatch(batch);
+    }
+
+    drawObject(entry) {
+        /** Draw an object.
+         *  @param {RomListEntry} entry Object to draw.
+         */
+        const gl = this.gx.gl;
+        const batch = new RenderBatch(this.gx);
+        const x = entry.position.x;
+        const y = entry.position.y;
+        const z = entry.position.z;
+        const s = Math.max(entry.object.scale, 10);
+        //console.log("draw obj", entry, "at", x, y, z, "scale", s)
+
+        let id = 0;
+        if(this._isDrawingForPicker) {
+            id = this._nextPickerId++;
+            this._pickerIds[id] = {
+                type: 'object',
+                obj: entry,
+            };
+        }
+
+        //just draw a cube
+        batch.addVertices(...this._makeCube(x, y, z, s, id));
+        return batch;
+    }
+
+    _makeCube(x, y, z, scale, id) {
+        const vtxPositions = [ //x, y, z
+            [x-scale, y-scale, z-scale], //[0] top left back
+            [x+scale, y-scale, z-scale], //[1] top right back
+            [x+scale, y+scale, z-scale], //[2] top right front
+            [x-scale, y+scale, z-scale], //[3] top left front
+            [x-scale, y-scale, z+scale], //[4] bot left back
+            [x+scale, y-scale, z+scale], //[5] bot right back
+            [x+scale, y+scale, z+scale], //[6] bot right front
+            [x-scale, y+scale, z+scale], //[7] bot left front
+        ];
+        const vtxColors = [ //r, g, b, a
+            [0x00, 0x00, 0x00, 0xCF], //[0] top left back
+            [0xFF, 0x00, 0x00, 0xCF], //[1] top right back
+            [0xFF, 0xFF, 0x00, 0xCF], //[2] top right front
+            [0x00, 0xFF, 0x00, 0xCF], //[3] top left front
+            [0x00, 0x00, 0xFF, 0xCF], //[4] bot left back
+            [0xFF, 0x00, 0xFF, 0xCF], //[5] bot right back
+            [0xFF, 0xFF, 0xFF, 0xCF], //[6] bot right front
+            [0x00, 0xFF, 0xFF, 0xCF], //[7] bot left front
+        ];
+        const vtxIdxs = [
+            0,1,3, 1,2,3, //top
+            3,2,6, 3,6,7, //front
+            0,3,4, 3,7,4, //left
+            2,1,5, 2,5,6, //right
+            7,5,4, 7,6,5, //bottom
+            1,0,5, 0,4,5, //back
+        ];
+        const vtxs = [this.gx.gl.TRIANGLES];
+        for(const idx of vtxIdxs) {
+            vtxs.push({
+                POS:  vtxPositions[idx],
+                COL0: vtxColors[idx],
+                COL1: vtxColors[idx],
+                id:   id,
+            });
+        }
+        return vtxs;
+    }
+}
