f4ae7bd2e8faf3e278e1f0b72c3a0635c73ec007
Rena Kunisaki <hyperhacker@gmail.com>
2022-06-01 20:27:12 -0400

    move speedrun to new repo

--- speedrun/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# build script for my own system
-# just moves some directories off a network mount for performance
-DISCROOT=/home/rena/projects/sfa/files ORIGISO=~/projects/sfa/original.iso PATCHISO=~/projects/sfa/patched.iso make $*
