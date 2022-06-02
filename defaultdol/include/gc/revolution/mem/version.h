aaa58c4a6868e147d491d4cc45cc7abcf10d5c83
Rena Kunisaki <hyperhacker@gmail.com>
2022-06-01 20:23:25 -0400

    move defaultdol to new repo

--- defaultdol/include/gc/revolution/mem/version.h
+++
@@ -1,3 +0,0 @@
-#ifndef __SDK_MEM_VERSION__
-#define __SDK_MEM_VERSION__  051115
-#endif
--- defaultdol/include/sfa/math.h
+++
@@ -1,3 +0,0 @@
-typedef struct { float x; float y; float z; } vec3f;
-typedef struct { short x; short y;          } vec2s;
-typedef struct { short x; short y; short z; } vec3s;
